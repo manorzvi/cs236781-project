1e4b2064202cbc58ff6d4e4b8c74f0d4c0e14293
TomAvrech <tomavrech@gmail.com>
2020-04-02 06:35:32 +0300

    Inference + Navigation

--- data_manager.py
+++ data_manager.py
@@ -14,26 +14,33 @@ from torch.utils.data import DataLoader
 def one_one_normalization(data):
     return 2*((data-np.min(data))/(np.max(data)-np.min(data)))-1
 
+
 def calc_grads(img: torch.Tensor, ksizes:list=[3,5], kweights=[0.7,0.5], grads_degree=1, normalization=True):
     assert isinstance(img,torch.Tensor)
     assert isinstance(ksizes,list) and isinstance(kweights, list)
+
     img_np = img.cpu().numpy().squeeze()
     # grad_x = cv2.Sobel(img_np, cv2.CV_64F, grads_degree, 0, ksize=ksizes[0])
     # grad_y = cv2.Sobel(img_np, cv2.CV_64F, 0, grads_degree, ksize=ksizes[0])
+
     for i,(size,weight) in enumerate(zip(ksizes,kweights)):
         if i == 0:
-            grad_x = weight * cv2.Sobel(img_np, cv2.CV_64F, grads_degree, 0, ksize=size)
-            grad_y = weight * cv2.Sobel(img_np, cv2.CV_64F, 0, grads_degree, ksize=size)
+            grad_x = cv2.Sobel(img_np, cv2.CV_64F, grads_degree, 0, ksize=size) * weight
+            grad_y = cv2.Sobel(img_np, cv2.CV_64F, 0, grads_degree, ksize=size) * weight
         else:
-            grad_x += weight * cv2.Sobel(img_np, cv2.CV_64F, grads_degree, 0, ksize=size)
-            grad_y += weight * cv2.Sobel(img_np, cv2.CV_64F, 0, grads_degree, ksize=size)
+            grad_x += cv2.Sobel(img_np, cv2.CV_64F, grads_degree, 0, ksize=size) * weight
+            grad_y += cv2.Sobel(img_np, cv2.CV_64F, 0, grads_degree, ksize=size) * weight
+
     if normalization:
         grad_x = one_one_normalization(grad_x)
         grad_y = one_one_normalization(grad_y)
+
     grad_x = np.expand_dims(grad_x, axis=2)
     grad_y = np.expand_dims(grad_y, axis=2)
+
     grad_x = T.ToTensor()(grad_x).type(torch.float32)
     grad_y = T.ToTensor()(grad_y).type(torch.float32)
+
     return grad_x,grad_y
 
 class RandomCropAndResize(object):
@@ -110,86 +117,142 @@ class RotateAndFillCornersWithImageFrameColors(object):
         return rgb, depth
 
 class rgbd_gradients_dataset(Dataset):
-    def __init__(self, root, image_size, constant_index, overfit_mode=False):
+    def __init__(self, root, image_size, constant_index=None, use_transforms=False, overfit_mode=False):
+        print(f'[I (rgbd_gradients_dataset)] - root={root}\n'
+              f'                             - image_size={image_size}\n'
+              f'                             - use_transforms={use_transforms}\n'
+              f'                             - overfit_mode={overfit_mode}\n'
+              f'                             - constant_index={constant_index}\n')
         self.root             = root
+        self.use_transforms   = use_transforms
         self.overfit_mode     = overfit_mode
         self.image_size       = image_size
-        self.rgbs   = list(sorted(os.listdir(os.path.join(root, 'rgb'))))# load all image files, sorting them to
-        self.depths = list(sorted(os.listdir(os.path.join(root, 'depth'))))# ensure that they are aligned
+        if constant_index:
+            self.constant_index = constant_index
+
+        # load all image files, sorting them to
+        # ensure that they are aligned
+        self.rgbs   = list(sorted(os.listdir(os.path.join(root, 'rgb'))))
+        self.depths = list(sorted(os.listdir(os.path.join(root, 'depth'))))
+        self.depth_inference_path = os.path.join(root, 'depth_inference/0.png')
+
         if not (len(self.rgbs) == len(self.depths)):
             raise Exception(f"Non-equal number of samples from each kind "
                             f"(|rgbs|={len(self.rgbs)} != |depths|={len(self.depths)})")
-        self.constant_index = constant_index
-        if 0 <= self.constant_index:
-            self.len = 1
-        else:
-            self.len = len(self.rgbs)
+
+        self.len = len(self.rgbs)
+        print(f'[I] - |self|={len(self)}')
+
     def transform(self, rgb, depth):
+        # TODO: I really don't understand why to apply every transform you found online,
+        #  while we still can't run a single proper training iteration.
+        
+        # Resize to constant spatial dimensions
+        rgb   = T.Resize(self.image_size)(rgb)
+        depth = T.Resize(self.image_size)(depth)
+            
         if not self.overfit_mode:
+            # Random horizontal flipping
             if 0.5 < random.random():
-                rgb = T.RandomHorizontalFlip(p=1.0)(rgb)
+                rgb   = T.RandomHorizontalFlip(p=1.0)(rgb)
                 depth = T.RandomHorizontalFlip(p=1.0)(depth)
-        # Randomly changes the brightness, contrast and saturation of an image.
-        # Example: https://discuss.pytorch.org/t/data-augmentation-in-pytorch/7925/15
-        rgb = T.ColorJitter(
-            brightness=abs(0.1 * torch.randn(1).item()),
-            contrast=abs(0.1 * torch.randn(1).item()),
-            saturation=abs(0.1 * torch.randn(1).item()),
-            hue=abs(0.1 * torch.randn(1).item())
-        )(rgb)
-        rgb, depth = RandomCropAndResize(output_size=self.image_size, minimum_image_precentage_to_look_at=0.7)(rgb, depth)
-        rgb, depth = RotateAndFillCornersWithImageFrameColors(output_size=self.image_size, degrees_range=20, frame_removal_length=3)(rgb, depth)
+
+        # TODO: Uncomment the following later, after we see some progress.
+        
+#         # Randomly changes the brightness, contrast and saturation of an image.
+#         # Example: https://discuss.pytorch.org/t/data-augmentation-in-pytorch/7925/15
+#         rgb = T.ColorJitter(
+#             brightness=abs(0.1 * torch.randn(1).item()),
+#             contrast=abs(0.1 * torch.randn(1).item()),
+#             saturation=abs(0.1 * torch.randn(1).item()),
+#             hue=abs(0.1 * torch.randn(1).item())
+#         )(rgb)
+        
+#         # Crops and resizes back to IMAGE_SIZE
+#         rgb, depth = RandomCropAndResize(output_size=IMAGE_SIZE, minimum_image_precentage_to_look_at=0.7)(rgb, depth)
+        
+#         # Rotation
+#         rgb, depth = RotateAndFillCornersWithImageFrameColors(output_size=IMAGE_SIZE, degrees_range=20, frame_removal_length=3)(rgb, depth)
+    
         # USE THIS ??? torchvision.transforms.functional.perspective(img, startpoints, endpoints, interpolation=3)
+        
+        # PIL.Image -> torch.Tensor
+        rgb   = T.ToTensor()(rgb)
+        depth = T.ToTensor()(depth)
+        
+        # Dynamic range [0,1] -> [-1, 1]
+        rgb   = T.Normalize(mean=(.5,.5,.5), std=(.5,.5,.5))(rgb)
+        depth = T.Normalize(mean=(.5,), std=(.5,))(depth)
+        
         return rgb, depth
     
     def __getitem__(self, index):
-        if 0 <= self.constant_index:
+        if hasattr(self,'constant_index'):
             index = self.constant_index
-        rgb   = Image.open(os.path.join(self.root, "rgb",   self.rgbs[index]))
-        d     = Image.open(os.path.join(self.root, "depth", self.depths[index]))
-        # Resize to constant spatial dimensions
-        rgb = T.Resize(self.image_size)(rgb)
-        d = T.Resize(self.image_size)(d)
-        rgb, d = self.transform(rgb, d)
-        rgb = T.ToTensor()(rgb)
-        rgb = T.Normalize(mean=(.5,.5,.5), std=(.5,.5,.5))(rgb) # Dynamic range [0,1] -> [-1, 1]
-        d = T.ToTensor()(d)
-        d = T.Normalize(mean=(.5,), std=(.5,))(d) # Dynamic range [0,1] -> [-1, 1]
+            print(f'[I] - index={index}')
+
+        rgb_path   = os.path.join(self.root, "rgb",   self.rgbs[index])
+        d_path     = os.path.join(self.root, "depth", self.depths[index])
+
+        rgb         = Image.open(rgb_path)
+        d           = Image.open(d_path)
+        d_inference = Image.open(self.depth_inference_path)
+
+        if self.use_transforms:
+            rgb, d = self.transform(rgb, d)
+            d_inference = T.Resize(self.image_size)(d_inference)
+            d_inference = T.ToTensor()(d_inference)
+            d_inference = T.Normalize(mean=(.5,), std=(.5,))(d_inference)
+
         x,y = calc_grads(d)
+
         return {'rgb': rgb,
                 'depth': d,
+                'depth_inference': d_inference,
                 'x'    : x,
                 'y'    : y}
 
     def __len__(self):
         return self.len
 
-def rgbd_gradients_dataloader(root, batch_size, num_workers, train_test_ratio, image_size, constant_index, evaluage=False, overfit_mode=False):
-    if 0 <= constant_index:
-        train_test_ratio=0.5
-    rgbd_grads_ds = rgbd_gradients_dataset(root, image_size, overfit_mode=overfit_mode, constant_index=constant_index)
-    if overfit_mode:
-        dl_overfit = torch.utils.data.DataLoader(rgbd_grads_ds,
-                                         batch_size=batch_size,
-                                         num_workers=num_workers,
-                                         shuffle=True)
-        return (dl_overfit, deepcopy(dl_overfit)) # dl_train & dl_test equals and consist of a single image.
-    else:
-        if 0 <= constant_index:
-            split_lengths = [0, 1]
-        else:
-            split_lengths = [int(np.ceil(len(rgbd_grads_ds)  *    train_test_ratio)),
-                             int(np.floor(len(rgbd_grads_ds) * (1-train_test_ratio)))]
+
+def rgbd_gradients_dataloader(root, batch_size, num_workers, train_test_ratio, image_size, constant_index,
+                              use_transforms=False, overfit_mode=False, seed=42):
+    print(f'[I (rgbd_gradients_dataloader)] - root={root}\n'
+          f'                                - batch_size={batch_size}\n'
+          f'                                - num_workers={num_workers}\n'
+          f'                                - train_test_ratio={train_test_ratio}\n'
+          f'                                - image_size={image_size}\n'
+          f'                                - use_transforms={use_transforms}\n'
+          f'                                - overfit_mode={overfit_mode}\n'
+          f'                                - seed={seed}\n'
+          f'                                - constant_index={constant_index}\n')
+    torch.manual_seed(seed)
+
+    rgbd_grads_ds = rgbd_gradients_dataset(root, image_size, use_transforms=use_transforms, overfit_mode=overfit_mode,
+                                           constant_index=constant_index)
+
+    if not overfit_mode:
+        split_lengths = [int(np.ceil(len(rgbd_grads_ds)  *    train_test_ratio)),
+                         int(np.floor(len(rgbd_grads_ds) * (1-train_test_ratio)))]
+
+        # NOTE: Don't forget to set the seed in order to maintain reproducibility over training experiments.
         ds_train, ds_test = random_split(rgbd_grads_ds, split_lengths)
-        if constant_index < 0:
-            dl_train = torch.utils.data.DataLoader(ds_train,
-                                                   batch_size=batch_size,
-                                                   num_workers=num_workers,
-                                                   shuffle=True)
-        else:
-            dl_train = None
+
+        print(f'[I (rgbd_gradients_dataloader)] - |Train Dataset|={len(ds_train)}, |Test Dataset|={len(ds_test)}')
+        dl_train = torch.utils.data.DataLoader(ds_train,
+                                               batch_size=batch_size,
+                                               num_workers=num_workers,
+                                               shuffle=True)
         dl_test  = torch.utils.data.DataLoader(ds_test,
                                                batch_size=batch_size,
                                                num_workers=num_workers,
                                                shuffle=True)
         return (dl_train, dl_test)
+    else:
+        dl_overfit = torch.utils.data.DataLoader(rgbd_grads_ds,
+                                                 batch_size=batch_size,
+                                                 num_workers=num_workers,
+                                                 shuffle=True)
+        print(f'[I (rgbd_gradients_dataloader)] - |Dataset|={len(rgbd_grads_ds)}')
+        return (dl_overfit, deepcopy(dl_overfit)) # dl_train & dl_test equals and consist of a single image.
