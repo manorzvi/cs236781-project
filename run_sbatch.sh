8e0dc9e67f1af49a836a081518304ba546866544
manorz <manor.zvi@campus.technion.ac.il>
2020-04-04 10:20:43 +0300

    train_gridsearch

--- run_sbatch.sh
+++ run_sbatch.sh
@@ -2,7 +2,9 @@
 
 # Setup env
 source $HOME/anaconda3/etc/profile.d/conda.sh
-conda activate cs236781-project
+#conda activate cs236781-project
+conda activate cs236781-hw
 echo "hello from $(python --version) in $(which python)"
 
-python train.py
+python -c 'import torch; print(f"i can haz gpu? {torch.cuda.is_available()}")'
+python train_gridsearch.py
